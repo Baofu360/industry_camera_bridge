2ee51877fb539c7307cb5bf7a99efbffdd6bee72
NunchakusLei <NunchakusLei@users.noreply.github.com>
2018-02-21 04:05:00 -0700

    Update camera_publisher.cpp
    combines the camera module parts.

--- src/camera_publisher.cpp
+++ src/camera_publisher.cpp
@@ -8,13 +8,14 @@
 int main(int argc, char** argv)
 {
   // Check if video source has been passed as a parameter
-  if(argv[1] == NULL) return 1;
+  //if(argv[1] == NULL) return 1;
 
   ros::init(argc, argv, "image_publisher");
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
+  /*
   // Convert the passed as command line parameter index for the video device to an integer
   std::istringstream video_sourceCmd(argv[1]);
   int video_source;
@@ -24,14 +25,53 @@ int main(int argc, char** argv)
   cv::VideoCapture cap(video_source);
   // Check if video device can be opened with the given index
   if(!cap.isOpened()) return 1;
+  */
   cv::Mat frame;
   sensor_msgs::ImagePtr msg;
 
-  // cv::namedWindow("test");
-  // cv::startWindowThread();
+  // camera settings
+  int                     iCameraCounts = 1;
+    int                     iStatus=-1;
+    tSdkCameraDevInfo       tCameraEnumList;
+    int                     hCamera;
+    tSdkCameraCapbility     tCapability;      // Device descriptions
+    tSdkFrameHead           sFrameInfo;
+    BYTE*			        pbyBuffer;
+    int                     iDisplayFrames = 10000;
+    IplImage *iplImage = NULL;
+    int                     channel=3;
+
+    CameraSdkInit(1);
+
+    // link devices
+    CameraEnumerateDevice(&tCameraEnumList,&iCameraCounts);
+    // No device connected
+    if(iCameraCounts==0){
+        return -1;
+    }
+  
+  // Initialize cameras
+    iStatus = CameraInit(&tCameraEnumList,-1,-1,&hCamera);
+    // Initialize cameras fail
+    if(iStatus!=CAMERA_STATUS_SUCCESS){
+        return -1;
+    }
+  
+  CameraGetCapability(hCamera,&tCapability);
+  g_pRgbBuffer = (unsigned char*)malloc(tCapability.sResolutionRange.iHeightMax*tCapability.sResolutionRange.iWidthMax*3);
+  CameraPlay(hCamera);
+  
+  if(tCapability.sIspCapacity.bMonoSensor){
+        channel=1;
+        CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_MONO8);
+    }else{
+        channel=3;
+        CameraSetIspOutFormat(hCamera,CAMERA_MEDIA_TYPE_BGR8);
+    }
 
   ros::Rate loop_rate(5);
   while (nh.ok()) {
+    /*
     cap >> frame;
     // Check if grabbed frame is actually full with some content
     if(!frame.empty()) {
@@ -39,6 +79,34 @@ int main(int argc, char** argv)
       pub.publish(msg);
       cv::waitKey(1);
     }
+    */
+    
+    if(CameraGetImageBuffer(hCamera,&sFrameInfo,&pbyBuffer,1000) == CAMERA_STATUS_SUCCESS)
+		{
+		    CameraImageProcess(hCamera, pbyBuffer, g_pRgbBuffer,&sFrameInfo);
+		    if (iplImage)
+            {
+                cvReleaseImageHeader(&iplImage);
+            }
+            iplImage = cvCreateImageHeader(cvSize(sFrameInfo.iWidth,sFrameInfo.iHeight),IPL_DEPTH_8U,channel);
+            cvSetData(iplImage,g_pRgbBuffer,sFrameInfo.iWidth*channel);
+            // convert iplImage to OpenCV Mat
+            #if 0
+            cvShowImage("OpenCV Demo",iplImage);
+            #else
+            cv::Mat Iimag(iplImage);
+            //imshow("OpenCV Demo",Iimag);
+            #endif
+
+            msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", Iimag).toImageMsg();
+            pub.publish(msg);
+             cv::waitKey(1);
+
+            // MUST release the buffer allocated from CameraReleaseImageBuffer after CameraGetImageBuffer.
+			//Otherwise next CameraGetImageBuffer will be blocked until CameraReleaseImageBuffer been called to release the buffer.
+			CameraReleaseImageBuffer(hCamera,pbyBuffer);
+
+		}
 
     ros::spinOnce();
     loop_rate.sleep();
